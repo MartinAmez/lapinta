5a7c6a5b6972ad3e4e79886c2d536a0fd40362fd
LucasZV <l.zamoratech@gmail.com>
2024-05-07 18:17:43 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -31,7 +31,7 @@ colaboradores = [
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1> Bienvenido a la API de 'LA PINTA'</h2><p> Esta API analiza el indice (contar un poco la historia...) <p> Para realizar una predicción escribe: --> 'https://lapinta1.pythonanywhere.com/api/v1/predict'.<p> Para reentrenar el modelo: --> 'https://lapinta1.pythonanywhere.com/api/v1/retrain'</p> <p> Para consultar los colaboradores: --> 'https://lapinta1.pythonanywhere.com/api/v1/colaboradores/all'</p>"
+    return "<h1> Bienvenido a la API de 'LA PINTA'</h2><p> Esta API utiliza un modelo de regresión XGBoost para predecir el ratio de suicidio por 100.000 habitantes <p> Para realizar una predicción escribe: --> 'https://lapinta1.pythonanywhere.com/api/v1/predict'.<p> Para reentrenar el modelo: --> 'https://lapinta1.pythonanywhere.com/api/v1/retrain'</p> <p> Para consultar los colaboradores: --> 'https://lapinta1.pythonanywhere.com/api/v1/colaboradores/all'</p>"
 # Poner aqui instrucciones de uso:
 
 ####################
