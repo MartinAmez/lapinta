c6aa5063faf5ae230bbe6c4e32ac3328b0389233
LucasZV <l.zamoratech@gmail.com>
2024-05-08 09:00:25 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -87,7 +87,7 @@ def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
     if None in [Year, SuicideCount, CauseSpecificDeathPercentage, Population, GDP, GDPPerCapita, InflationRate, EmploymentPopulationRatio, regionname_num, sex_num, agegroup_num, countryname_num]:
         return "Args empty, the data are not enough to predict"
     else:
-        prediction = model.predict([Year, SuicideCount,CauseSpecificDeathPercentage, Population, GDP, GDPPerCapita, InflationRate, EmploymentPopulationRatio, regionname_num,sex_num, agegroup_num, countryname_num])
+        prediction = model.predict([[np.int64(Year), np.float64(SuicideCount), np.float64(CauseSpecificDeathPercentage), np.float64(Population), np.float64(GDP), np.float64(GDPPerCapita), np.float64(InflationRate), np.float64(EmploymentPopulationRatio), np.float64(regionname_num), np.int64(sex_num), np.int64(agegroup_num), np.int64(countryname_num)]])
         prediction_float = np.float64(prediction[0])
         return jsonify({'predictions': float(prediction_float)})
 
